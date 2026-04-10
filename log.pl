00ba17b1f11f548fafef409958e67e542910269d
CrazySpence <phil@philtopia.com>
2026-04-10 12:23:14 -0400

    Save log output to daily rotating log files in logs/
    do_log() now writes to both stdout and ./logs/ctf-YYYY-MM-DD.log.
    File rotates automatically at midnight by detecting the date change.
    Log directory is created on first write if it does not exist.
    logs/ added to .gitignore so log files are not committed.

--- log.pl
+++ log.pl
@@ -1,12 +1,39 @@
 #!/usr/bin/perl
 # log
 #
-# Log data to strerr or file
+# Log data to stdout and a rotating daily log file
+
+use POSIX qw(strftime);
+
+my $LOG_FH;
+my $LOG_DATE = "";
+
+sub log_open {
+    my $date = strftime("%Y-%m-%d", localtime());
+    return if $LOG_DATE eq $date && defined $LOG_FH;
+
+    # Close previous handle if open (date rolled over)
+    if (defined $LOG_FH) {
+        close $LOG_FH;
+        undef $LOG_FH;
+    }
+
+    my $filename = "./logs/ctf-$date.log";
+    mkdir "./logs" unless -d "./logs";
+    open($LOG_FH, '>>', $filename) or warn "ERROR -> Cannot open log file $filename: $!\n";
+    if ($LOG_FH) {
+        $LOG_FH->autoflush(1);
+        $LOG_DATE = $date;
+    }
+}
 
 sub do_log #($data)
 {
    my $data = $_[0];
-   print STDOUT "[" . scalar localtime() . "] " . $data . "\n";
+   my $line = "[" . scalar localtime() . "] " . $data . "\n";
+   print STDOUT $line;
+   log_open();
+   print $LOG_FH $line if defined $LOG_FH;
 }
 
 return true;
